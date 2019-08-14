7d0c09c9dd8d37e90265864e7f2e1eb16ced3a84
Warren Loy Harper II <warrenharper2@att.net>
2019-08-14 18:45:43 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,12 +3,24 @@ def speak_to_grandma(phrase)
     if phrase == "I LOVE YOU GRANDMA!"
       return "I LOVE YOU TOO PUMPKIN!"
   
+<<<<<<< HEAD
     elsif phrase != phrase.upcase
   		return "HUH?! SPEAK UP, SONNY!"
   
     else 
+=======
+    elsif phrase == phrase.downcase
+  		return "HUH?! SPEAK UP, SONNY!"
+  
+    else phrase == phrase.upcase
+>>>>>>> 3d5ed7a87430a180d70f68141cfc68bda8f19c1f
   		return "NO, NOT SINCE 1938!"
     end
 end
 
+<<<<<<< HEAD
 
+=======
+speak_to_grandma("I LOVE YOU GRANDMA!")
+  
+>>>>>>> 3d5ed7a87430a180d70f68141cfc68bda8f19c1f
